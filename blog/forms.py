21cbd149a489b05c0d3ac7b7c1a7066a3160909d
afruzbeksaydullayev <kafruzbe3@gmail.com>
2024-06-07 00:09:18 +0500

    60 ball qoysez ham roziman 😊

---
+++ blog/forms.py
@@ -0,0 +1,25 @@
+from django import forms # type: ignore
+from .models import Product, Customer
+
+
+
+class ProductForm(forms.ModelForm):
+    delete = forms.BooleanField(required=False, initial=False)
+
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'price',
+            'rating',
+            'amount',
+            'discount',
+            'attribute',
+        ]
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'phone', 'address']
+
